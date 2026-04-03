4ec5f77b4076a1ed14254ebe3aae33e13064f01d
Nunu-DMG09 <davidmesta09@gmail.com>
2026-04-03 17:24:43 -0500

    refactor TeamRepository interface by removing comments for clarity

--- backend/src/teams/domain/TeamRepository.ts
+++ backend/src/teams/domain/TeamRepository.ts
@@ -3,8 +3,9 @@ import { Team, TeamMember } from './Team';
 export interface TeamRepository {
   findByName(name: string): Promise<Team | null>;
   findById(id: number): Promise<Team | null>;
-  createTeam(team: Team): Promise<number>; // Devuelve el ID del equipo creado
+  createTeam(team: Team): Promise<number>; 
   addMember(member: TeamMember): Promise<void>;
-  getMembersByTeam(teamId: number): Promise<any[]>; // Traerá info del usuario también
+  getMembersByTeam(teamId: number): Promise<any[]>; 
   isUserInAnyTeam(userId: number): Promise<boolean>;
+  findAll(): Promise<any[]>
 }
