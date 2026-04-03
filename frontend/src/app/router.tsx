34cb52a600222213f5f6fe3f4b0f64186b2703fa
Nunu-DMG09 <davidmesta09@gmail.com>
2026-04-03 17:25:50 -0500

    add routes for teams and tournaments in the router configuration

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -5,6 +5,16 @@ import AppLayout from '../shared/components/layout/AppLayout';
 import UsersList from '../features/users/components/UsersList';
 import UserCreateView from '../features/users/components/UserCreateView';
 
+import TeamsList from '../features/teams/components/TeamsList';
+import TeamCreateView from '../features/teams/components/TeamCreateView';
+import TeamDetailView from '../features/teams/components/TeamDetailView';
+
+import TournamentsList from '../features/tournaments/components/TournamentsList';
+import TournamentCreateView from '../features/tournaments/components/TournamentCreateView';
+
+import TournamentDetailView from '../features/tournaments/components/TournamentDetailView';
+import HallOfFameView from '../features/hallOfFame/components/HallOfFameView';
+
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -12,22 +22,36 @@ export const router = createBrowserRouter([
   },
   {
     path: '/',
-    element: <AppLayout />, // El Layout envuelve estas rutas
+    element: <AppLayout />, 
     children: [
       {
         path: 'dashboard',
         element: <DashboardView />,
       },
       {
-        path: 'users', // <--- NUEVA RUTA
+        path: 'users', 
         element: <UsersList />,
       },
       { path: 'users/new',
         element: <UserCreateView /> 
       },
-      // Aquí iremos agregando:
-      // { path: 'tournaments', element: <TournamentsList /> },
-      // { path: 'teams', element: <TeamsList /> },
+
+      { path: 'teams',
+        element: <TeamsList />
+      }, 
+      { path: 'teams/new',
+        element: <TeamCreateView />
+      },
+      {
+        path: 'teams/:id', 
+        element: <TeamDetailView />,
+      },
+
+      { path: 'tournaments', element: <TournamentsList /> },
+      { path: 'tournaments/new', element: <TournamentCreateView /> },
+      { path: 'tournaments/:id', element: <TournamentDetailView /> },
+      { path: 'hall-of-fame', element: <HallOfFameView /> },
+      
     ],
   },
 ]);
