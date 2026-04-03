46fc7b7365c588533a46c4536caf28436df930ee
Nunu-DMG09 <davidmesta09@gmail.com>
2026-04-02 20:48:11 -0500

    feat: add routing for users and user creation views

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginView from '../features/auth/components/LoginView';
 import DashboardView from '../features/dashboard/components/DashboardView';
+import AppLayout from '../shared/components/layout/AppLayout';
+import UsersList from '../features/users/components/UsersList';
+import UserCreateView from '../features/users/components/UserCreateView';
 
 export const router = createBrowserRouter([
   {
@@ -8,8 +11,23 @@ export const router = createBrowserRouter([
     element: <LoginView />,
   },
   {
-    path: '/dashboard',
-    element: <DashboardView />,
+    path: '/',
+    element: <AppLayout />, // El Layout envuelve estas rutas
+    children: [
+      {
+        path: 'dashboard',
+        element: <DashboardView />,
+      },
+      {
+        path: 'users', // <--- NUEVA RUTA
+        element: <UsersList />,
+      },
+      { path: 'users/new',
+        element: <UserCreateView /> 
+      },
+      // Aquí iremos agregando:
+      // { path: 'tournaments', element: <TournamentsList /> },
+      // { path: 'teams', element: <TeamsList /> },
+    ],
   },
-  // Más adelante agregaremos /tournaments, /teams, /hall-of-fame
 ]);
