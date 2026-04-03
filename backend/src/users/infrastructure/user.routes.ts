fb405d22b67e2358ee8a72d16bb2b2b00081bb1b
Nunu-DMG09 <davidmesta09@gmail.com>
2026-04-02 20:47:31 -0500

    add GET route to retrieve all users

--- backend/src/users/infrastructure/user.routes.ts
+++ backend/src/users/infrastructure/user.routes.ts
@@ -13,7 +13,7 @@ const controller = new UserController(
   new UpdateUserUseCase(repo),
   new DeleteUserUseCase(repo)
 );
-
+router.get('/', controller.getAll.bind(controller));
 router.post('/', controller.create.bind(controller));
 router.put('/:id', controller.update.bind(controller));
 router.delete('/:id', controller.delete.bind(controller));
